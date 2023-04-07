6a88b5a1d5b9e0327ac2e049b1e82e2dbf5a89d8
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-08 02:07:19 +0800

    auto commit

--- cp_cheater.sh
+++ cp_cheater.sh
@@ -1,5 +1,5 @@
 echo "Check Src"
 ls /home/data/1945 | wc -l
-cp -ar /home/data/1945 /project/train/src_repo/1945
+cp -ar /home/data/1945 /home/dd
 echo "Copy Done"
-ls /project/train/src_repo/1945 | wc -l
+ls /home/dd | wc -lservice sshd start
