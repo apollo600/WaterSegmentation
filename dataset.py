6202519cf42600ec8e271121dd680cca0c438978
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-10 22:22:05 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -2,4 +2,7 @@ from torch.utils.data.dataset import Dataset
 
 class Dataset(Dataset):        
     def __init__(self, path, augmentation=False):                
-        super
+        super().__init__()
+        self.path = path
+
+        self.data = 
