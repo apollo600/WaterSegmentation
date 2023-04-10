48e230ce1da5c570db28dc55649fbdd1842e4827
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-10 22:30:23 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -1,8 +1,9 @@
 from torch.utils.data.dataset import Dataset
 
+# tmp not use
 class Dataset(Dataset):        
     def __init__(self, path, augmentation=False):                
         super().__init__()
         self.path = path
 
-        self.data = 
+        
