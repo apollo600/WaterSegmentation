41cfb80d158094b604d8db3d5b4e74a8a9694b31
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-10 22:05:51 +0800

    auto commit

--- test.py
+++ test.py
@@ -60,5 +60,26 @@ class UNET(nn.Module):
             [(1024, 512), (512, 256), (256, 128), (128, 64)]])
         self.up_conv = nn.ModuleList([DoubleConvolution(i, o) for i, o in
             [(1024, 512), (512, 256), (256, 128), (128, 64)]])
-        self.concat = 
-                                    
+        self.concat = nn.ModuleList([CropAndConcat() for _ in range(4)])
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        # collect the outputs of contracting path for later concatenation with the expansive path
+        pass_through = []
+
+        for i in range(len(self.down_conv)):
+            x = self.down_conv[i](x)
+            pass_through.append(x)
+            x = self.donw_sample[i](x)
+        
+        x = self.middle_conv(x)
+
+        for i in range(len(self.up_conv)):
+            x = self.up_sample[i](x)
+            x = self.concat[i](x, pass_through.pop())
+            x = self.up_conv[i](x)
+
+        x = self.final_conv(x)
+
+        return x
+                                                
