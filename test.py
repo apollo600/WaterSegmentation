94b71aaffe07e94895c8e32b114ecc4430b7ec55
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-10 21:35:50 +0800

    auto commit

--- test.py
+++ test.py
@@ -15,4 +15,28 @@ class DoubleConvolution(nn.Module):
 
     def forward(self, x: torch.Tensor()):        
         x = self.first(x)
-        x = self.                                                                                    
+        x = self.act1(x)
+        x = self.second(x)
+        x = self.act2(x)
+        return x
+
+
+class DownSample(nn.Module):
+    def __init__(self, x):
+        super().__init__()
+
+        self.pool = nn.MaxPool2d(kernel_size=2)
+    
+    def forward(self, x: torch.Tensor):
+        return self.pool(x)
+
+
+class UpSample(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+
+    def forward(self, x: torch.Tensor):
+        return self.up(x)
+                    
