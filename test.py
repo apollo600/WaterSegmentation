668f64ef1a91c39021dfe61e1776ee68da67f38b
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-10 21:30:28 +0800

    auto commit

--- test.py
+++ test.py
@@ -3,3 +3,16 @@ import torchvision.transforms.functional
 from torch import nn
 
 
+class DoubleConvolution(nn.Module):            
+    def __init__(self, in_channels: int, out_channels: int):            
+        super().__init__()
+
+        self.first = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.act1 = nn.ReLU()
+
+        self.second = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.act2 = nn.ReLU()
+
+    def forward(self, x: torch.Tensor()):        
+        x = self.first(x)
+        x = self.                                                                                    
