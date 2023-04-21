890a159bee1c4d2bcf0b2d591668c2a1ad2f42a1
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-21 20:55:01 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -4,6 +4,7 @@ import cv2
 from tqdm import tqdm
 import numpy as np
 import torch
+from PIL import Image
 
 # tmp not use
 class MyData(Dataset):        
@@ -22,15 +23,19 @@ class MyData(Dataset):
         print(f"Found {len(file_list) // 2} images")
 
     def __getitem__(self, index):
-        image = cv2.imread(os.path.join(self.path, self.file_list[2 * index + 1]))
-        image = cv2.resize(image, (self.image_width, self.image_height))
-        image = np.transpose(image, [2, 0, 1])
-        image = image.astype(np.float32)
-        image = torch.tensor
-        label = cv2.imread(os.path.join(self.path, self.file_list[2 * index]))
-        label = cv2.resize(label, (self.image_width, self.image_height))
-        label = np.transpose(label, [2, 0, 1])
-        label = label.astype(np.float32)
+        # image = cv2.imread(os.path.join(self.path, self.file_list[2 * index + 1]))
+        # image = cv2.resize(image, (self.image_width, self.image_height))
+        # image = np.transpose(image, [2, 0, 1])
+        # image = image.astype(np.float32)
+        # image = torch.from_numpy(image)
+        # label = cv2.imread(os.path.join(self.path, self.file_list[2 * index]))
+        # label = cv2.resize(label, (self.image_width, self.image_height))
+        # label = np.transpose(label, [2, 0, 1])
+        # label = label.astype(np.float32)
+        # label = torch.from_numpy(label)
+        image = Image.open(os.path.join(self.path, self.file_list[2 * index + 1]))
+        label = Image.open(os.path.join(self.path, self.file_list[2 * index]))
+        
         return image, label
 
     def __len__(self):
@@ -41,5 +46,4 @@ if __name__ == "__main__":
     train_dataset = MyData("/home/data/1945", image_width=720, image_height=540)
     image, label = train_dataset[0]
     print(image.size)
-    print()
     
