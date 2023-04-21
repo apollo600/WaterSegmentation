2dc548855e3049231224bf1027d0eca9f20cf932
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-21 20:25:01 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -12,27 +12,39 @@ class MyData(Dataset):
         self.path = path
         self.data = []
         self.label = []
+        self.image_width = image_width
+        self.image_height = image_height
 
         file_list = os.listdir(self.path)
         file_list.sort()
+        self.file_list = file_list
         assert(len(file_list) % 2 == 0)
         print(f"Found {len(file_list) // 2} images")
-        for i in tqdm(range(len(file_list) // 2), desc="Read: "):
-            image = cv2.imread(os.path.join(self.path, file_list[2 * i + 1]))
-            image = cv2.resize(image, (image_width, image_height))
-            image = np.transpose(image, [2, 0, 1])
-            image = image.astype(np.float32)
-            label = cv2.imread(os.path.join(self.path, file_list[2 * i]))
-            label = cv2.resize(label, (image_width, image_height))
-            label = np.transpose(label, [2, 0, 1])
-            label = label.astype(np.float32)
-            self.data.append(image)
-            self.label.append(label)
+        # for i in tqdm(range(len(file_list) // 2), desc="Read: "):
+        #     image = cv2.imread(os.path.join(self.path, file_list[2 * i + 1]))
+        #     image = cv2.resize(image, (image_width, image_height))
+        #     image = np.transpose(image, [2, 0, 1])
+        #     image = image.astype(np.float32)
+        #     label = cv2.imread(os.path.join(self.path, file_list[2 * i]))
+        #     label = cv2.resize(label, (image_width, image_height))
+        #     label = np.transpose(label, [2, 0, 1])
+        #     label = label.astype(np.float32)
+        #     self.data.append(image)
+        #     self.label.append(label)
 
     def __getitem__(self, index):
-        data = self.data[index]
-        label = self.label[index]
-        return data, label
+        image = cv2.imread(os.path.join(self.path, self.file_list[2 * index + 1]))
+        image = cv2.resize(image, (self.image_width, self.image_height))
+        image = np.transpose(image, [2, 0, 1])
+        image = image.astype(np.float32)
+        label = cv2.imread(os.path.join(self.path, self.file_list[2 * index]))
+        label = cv2.resize(label, (self.image_width, self.image_height))
+        label = np.transpose(label, [2, 0, 1])
+        label = label.astype(np.float32)
+        # data = self.data[index]
+        # label = self.label[index]
+        return image, label
 
     def __len__(self):
-        return len(self.data)
+        # return len(self.data)
+        return len(self.file_list) // 2
