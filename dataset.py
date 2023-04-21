7895d6dc8751f79bb056ec667d899e457d09d37d
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-21 10:50:01 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 from torch.utils.data.dataset import Dataset
+import os
+import cv2
 
 # tmp not use
-class Dataset(Dataset):        
+class MyData(Dataset):        
     def __init__(self, path, augmentation=False):                
         super().__init__()
         self.path = path
@@ -9,6 +11,12 @@ class Dataset(Dataset):
         self.label = []
 
         file_list = os.listdir(self.path)
-        file_prefix = [file.split]
+        file_list.sort()
+        assert(len(file_list) % 2 == 0)
+        print(f"Found {len(file_list) // 2} images")
+        for i in range(len(file_list) // 2):
+            image = cv2.imread(os.path.join(self.path, file_list[2 * i + 1]))
+            label = cv2.imread(os.path.join(self.path, file_list[2 * i]))
+        
 
         
