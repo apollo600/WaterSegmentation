3fdea2ef7882616c9000a148a6b71577efdeca1a
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-21 10:55:02 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data.dataset import Dataset
 import os
 import cv2
+from tqdm import tqdm
 
 # tmp not use
 class MyData(Dataset):        
@@ -14,9 +15,18 @@ class MyData(Dataset):
         file_list.sort()
         assert(len(file_list) % 2 == 0)
         print(f"Found {len(file_list) // 2} images")
-        for i in range(len(file_list) // 2):
+        for i in tqdm(range(len(file_list) // 2), desc="Read: "):
             image = cv2.imread(os.path.join(self.path, file_list[2 * i + 1]))
             label = cv2.imread(os.path.join(self.path, file_list[2 * i]))
-        
+            self.data.append(image)
+            self.label.append(label)
 
+    def __getitem__(self, index):
+        data = self.data[index]
+        label = self.label[index]
+        return data, label
+
+    def __len__(self):
+        return len(self.data)
+        
         
