c3dd282b3b34b7f029d5b985dee4cac8b7ee9b3b
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-21 11:45:02 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import os
 import cv2
 from tqdm import tqdm
 import numpy as np
+import torch
 
 # tmp not use
 class MyData(Dataset):        
@@ -31,5 +32,17 @@ class MyData(Dataset):
         return len(self.data)
         
 
-def myCollate(batch):            
-    return batch
+def myCollate(data):            
+    # 这里的data是一个list， list的元素是元组，元组构成为(self.data, self.label)
+	# collate_fn的作用是把[(data, label),(data, label)...]转化成([data, data...],[label,label...])
+	# 假设self.data的一个data的shape为(channels, length), 每一个channel的length相等,data[索引到数据index][索引到data或者label][索引到channel]
+    # data.sort(key=lambda x: len(x[0][0]), reverse=False)  # 按照数据长度升序排序
+    data_list = []
+    label_list = []
+    for batch in range(0, len(data)): #
+        data_list.append(data[batch][0])
+        label_list.append(data[batch][1])
+    data_tensor = torch.tensor(data_list, dtype=torch.float32)
+    label_tensor = torch.tensor(label_list, dtype=torch.float32)
+    data_copy = (data_tensor, label_tensor)
+    return data_copy
