b4cd9004a8009dcb04bf8d0a6700053ff133fe1c
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-23 20:00:14 +0800

    auto commit

--- dataset.py
+++ dataset.py
@@ -11,15 +11,15 @@ from PIL import Image
 class MyData(Dataset):        
     def __init__(self, path, image_width, image_height, augmentation=False):                
         super().__init__()
-        self.path = path
-        self.data = []
-        self.label = []
+        self.image_paths = []
+        self.label_paths = []
         self.image_width = image_width
         self.image_height = image_height
 
         file_list = os.listdir(self.path)
         file_list = [ x[:-4] for x in file_list if x.endswith('png') ]
-        self.file_list = file_list
+        self.image_paths = [ x + ".jpg" for x in file_list ]
+        self.label_paths = [ x + ".png" for x in file_list ]
         print(f"Found {len(file_list)} images")
 
     def __getitem__(self, index):
@@ -27,15 +27,17 @@ class MyData(Dataset):
             transforms.ToTensor()
         ])
 
-        image = cv2.imread(os.path.join(self.path, self.file_list[index] + ".jpg"))
+        image = Image.open(os.path.join(self.path, self.image_paths[index]))
+        image = np.array(image)
         image = cv2.resize(image, (self.image_width, self.image_height))
         image = np.transpose(image, [2, 0, 1])
         image = image.astype(np.float32)
         image = torch.from_numpy(image)
 
-        label = cv2.imread(os.path.join(self.path, self.file_list[index] + ".png"), -1)
+        label = Image.open(os.path.join(self.path, self.label_paths[index]))
+        label = np.array(label)
         label = cv2.resize(label, (self.image_width, self.image_height))
-        
+
         label = torch.from_numpy(label)
         
         return image, label
