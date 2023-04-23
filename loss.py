4053ee8a0c995191c917f7be16961f4a5ac0e24c
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-23 19:41:10 +0800

    auto commit

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class FocalLoss(nn.Module):                                                                                                      
@@ -6,4 +7,16 @@ class FocalLoss(nn.Module):
         self.gamma = gamma
         self.alpha = alpha
         if isinstance(alpha, (float, int, long)):
-            self.alpha = torch.Tensor                                                                                                                                                                                
+            self.alpha = torch.Tensor([alpha, 1-alpha])
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            # N, C, H, W => N, C, H*W
+            input = input.view(input.size(0), input.size(1), -1)
+            # N, C, H*W => N, C, H, W     
+            input = input.transpose(1, 2)     
+            # N, C, H*W => N*H*W, C
+            input = input.contiguous().view(-1, input.size(2))                                                                                                                                                                                                                                                                   
