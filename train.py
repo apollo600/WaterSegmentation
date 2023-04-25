5d72a6922e41a56c2adbc13b32bbefd41f5eb258
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-26 01:25:50 +0800

    auto commit

--- train.py
+++ train.py
@@ -76,25 +76,32 @@ def train(train_loader, train_model, args):
 
         print("Start Test")
         with torch.no_grad():
+            batches = len(train_loader)
+            total_acc = 0
+            pbar = tqdm(total=batches, maxinterval=0.3, ascii=True)
             for iteration, (data, label) in enumerate(val_loader):
                 data, label = data, label
                 pred_label = model(data)
             
-            # copy the tensor to host memory first
-            t_pred_label = pred_label.cpu().detach().numpy()
-            t_label = label.cpu().detach().numpy()
-            # get max arg as output label
-            t_pred_label = np.argmax(t_pred_label, axis=1)
-            t_label = np.transpose(t_label, [0, 3, 1, 2]).argmax(axis=1)
-            print(t_pred_label.shape, t_label.shape)
-            # update accuracy
-            acc = np.sum(t_label == t_pred_label) / np.prod(t_label.shape)
-            if acc > best_acc: 
-                print(f"Update acc {best_acc:.4f} => {acc:.4f}")
-                best_acc = acc
-                model_name = os.path.join(log_dir, f"best_acc-{acc:.4f}.pt")
+                # copy the tensor to host memory first
+                t_pred_label = pred_label.cpu().detach().numpy()
+                t_label = label.cpu().detach().numpy()
+                # get max arg as output label
+                t_pred_label = np.argmax(t_pred_label, axis=1)
+                t_label = np.transpose(t_label, [0, 3, 1, 2]).argmax(axis=1)
+                print(t_pred_label.shape, t_label.shape)
+                # update accuracy
+                acc = np.sum(t_label == t_pred_label) / np.prod(t_label.shape[1:])
+                total_acc += acc
+            total_acc /= batches
+            if total_acc > best_acc: 
+                print(f"Update acc {best_acc:.4f} => {total_acc:.4f}")
+                best_acc = total_acc
+                model_name = os.path.join(log_dir, f"best_acc-{total_acc:.4f}.pt")
                 torch.save(train_model.state_dict(), model_name)
-        
+            else:
+                
+            
 
 if __name__ == "__main__":            
     args = get_parser()
@@ -113,7 +120,7 @@ if __name__ == "__main__":
 
     # Create the loaders
     train_loader = DataLoader(train_dataset, batch_size=args.batch, shuffle=True, num_workers=0)
-    val_loader = DataLoader(val_dataset, batch_size=val_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
 
     # Create the model
     print("Loading model to device")
