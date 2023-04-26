8fafd38e8372d5b329d0a44e02436b702633b3d1
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-27 04:08:40 +0800

    backup test file ji.py

---
+++ ji.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import json
+import argparse
+ 
+from PIL import Image
+import numpy as np
+import cv2
+import torch
+
+sys.path.append("/project/train/src_repo/")
+
+from model import UNET
+ 
+def get_parser():                        
+    parser = argparse.ArgumentParser(description='Test UNET')
+    parser.add_argument("--model_path", type=str, default="/project/train/models/2023-04-26-15:26:55_epoch-100_lr-0.0005_loss-CrossEntropy_optim-AdamW_best_acc-0.7752.pt", help="path of model static dict to load")
+    parser.add_argument("--image_width", type=int, default=640)
+    parser.add_argument("--image_height", type=int, default=640)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def checkGPU():
+    gpu_count = torch.cuda.device_count()
+
+    print(">>> GPU List <<<")
+    for i in range(gpu_count):
+        print(i+1, torch.cuda.get_device_name(i))
+        
+
+def init():
+    
+    """Initialize model
+ 
+    Returns: model
+ 
+    """
+
+    args = get_parser()
+
+    checkGPU()
+    
+    print("Load Model")
+    model = UNET(3, 5)
+    if args.model_path is not None and args.model_path is not "":
+        model.load_state_dict(torch.load(args.model_path))
+    model = model.cuda()
+
+    return model
+ 
+def process_image(handle=None, input_image=None, args=None, **kwargs):
+    
+    """Do inference to analysis input_image and get output
+ 
+    Attributes:
+        handle: algorithm handle returned by init()
+        input_image (numpy.ndarray): image to be process, format: (h, w, c), BGR
+        args: string in JSON format, format: {
+            "mask_output_path": "/path/to/output/mask.png"
+        }
+ 
+    Returns: process result
+ 
+    """
+
+    args = json.loads(args)
+    mask_output_path = args['mask_output_path']
+    model = handle
+    
+    # Process image here
+    image = input_image
+    # H, W, C -> C, H, W
+    image = np.transpose(image, [2, 0, 1])
+    # C, H, W -> 1, Channels, H, W
+    image = np.expand_dims(image, axis=0)
+    image = torch.from_numpy(image).float()
+    image = image.cuda()
+    # 1, Classes, H, W
+    pred_label = model(image)
+    # Generate dummy mask data
+    t_pred_label = pred_label.cpu().detach().numpy()
+    # 1, C, H, W -> 1, H, W
+    t_pred_label = np.argmax(t_pred_label, axis=1)
+    # 1, H, W -> H, W
+    t_pred_label = np.squeeze(t_pred_label, axis=0)
+    # h, w, _ = input_image.shape
+    # dummy_data = np.random.randint(low=0, high=2, size=(w, h), dtype=np.uint8)
+    dummy_data = t_pred_label.astype(np.uint8)
+    pred_mask_per_frame = Image.fromarray(dummy_data)
+    pred_mask_per_frame.save(mask_output_path)
+    return json.dumps({'mask': mask_output_path}, indent=4)
+
+
+if __name__ == "__main__":        
+    args = get_parser()
+    
+    # 2023-04-26-15:26:55_epoch-100_lr-0.0005_loss-CrossEntropy_optim-AdamW_best_acc-0.7752.pt
+    model = init()
+
+    from dataset import MyData
+    from tqdm import tqdm
+    dataset = MyData("/home/data/1945", 5, args.image_width, args.image_height, is_train=False, one_hot=False)
+    total_acc = 0
+    for i in tqdm(range(len(dataset)), desc="Inferencing", ascii=True):
+        image, label = dataset[i]
+        # C, H, W -> H, W, C
+        image = image.transpose([1, 2, 0])
+
+        output_json = process_image(model, image, 
+            '{"mask_output_path": "/project/ev_sdk/mask.png"}')
+
+        pred_label = Image.open("/project/ev_sdk/mask.png")
+        pred_label = np.array(pred_label)
+
+        total_acc += (pred_label == label).sum() / np.prod(pred_label.shape)
+    print("Test acc:", total_acc / len(dataset))
