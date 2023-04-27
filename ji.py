77d86cdb65494f0272a667d9a4145ef06b684afd
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-27 23:30:03 +0800

    auto commit

--- ji.py
+++ ji.py
@@ -97,7 +97,8 @@ def process_image(handle=None, input_image=None, args=None, **kwargs):
 if __name__ == "__main__":        
     args = get_parser()
     
-    # 2023-04-26-15:26:55_epoch-100_lr-0.0005_loss-CrossEntropy_optim-AdamW_best_acc-0.7752.pt
+    # Best model now: 2023-04-26-15:26:55_epoch-100_lr-0.0005_loss-CrossEntropy_optim-AdamW_best_acc-0.7752.pt
+    # Best local model: 
     model = init()
 
     from dataset import MyData
