17127d14f9ba161860dc6031bea680f5ac51564f
Tastror <yccozy2001@163.com>
2023-04-28 01:09:13 +0800

    sh and bash update done

--- train.bat
+++ train.bat
@@ -1,22 +1,48 @@
 @echo off
 
+
+@REM paths
+set dataset_root=./
+set save_root=./
+set dataset_dir=1945/
+set save_dir=models/
+
+@REM dataset properties
+set dataset_name=My
+set num_classes=5
+
+@REM training methods
+set loss=CrossEntropy
+set lr=0.0005
+set batch_size=2
+set epoch=100
+
+
 set argc=0
 for %%x in (%*) do Set /A argc+=1
-
-set res=My
 if %argc% gtr 0 (
-    set res=%1
+    if "%1" == "Kitti" (
+        @REM change paths
+        set dataset_root=./
+        set dataset_dir=data_semantics/
+
+        @REM change dataset properties
+        set dataset_name=%1
+        set num_classes=34
+    )
 )
+
 
 @echo on
 
 python ./train.py ^
-    --data_root . ^
-    --save_root . ^
-    --dataset %res% ^
-    --loss CrossEntropy ^
-    --lr 0.0005 ^
-    --batch_size 2 ^
-    --epoch 100 ^
-    --save_dir models ^
-    --num_classes 5
+    --data_root %dataset_root% ^
+    --data_dir %dataset_dir% ^
+    --save_root %save_root% ^
+    --save_dir %save_dir% ^
+    --dataset %dataset_name% ^
+    --num_classes %num_classes% ^
+    --loss %loss% ^
+    --lr %lr% ^
+    --batch_size %batch_size% ^
+    --epoch %epoch%
