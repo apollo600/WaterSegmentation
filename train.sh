d87e602dccb177f3c1683c343d4206734949ba05
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-28 02:28:33 +0800

    update args

--- train.sh
+++ train.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 
-# paths
-dataset_root=/home/data/
-save_root=/project/train/
-dataset_dir=1945/
-save_dir=models/
-
 # dataset properties
 dataset_name=My
 num_classes=5
 
+# paths
+dataset_root=/home/data/
+dataset_dir=1945/
+save_root=/project/train/
+save_dir=models/
+
 # training methods
 loss=CrossEntropy
 lr=0.0005
-batch_size=2
+batch_size=8
 epoch=100
+image_width=384
+image_height=384
+optimizer=Adagrad
 
 
 if [[ $# -gt 0 ]] ; then
     if [[ $1 == "Kitti" ]] ; then
-        # change paths
-        dataset_root=/project/train/src_repo
-        dataset_dir=data_semantics/
-
         # change dataset properties
         dataset_name=$1
         num_classes=34
+
+        # change paths
+        dataset_root=/project/train/src_repo
+        dataset_dir=data_semantics/
     fi
 fi
 
@@ -35,15 +38,15 @@ set -x
 
 python ./train.py \
     --dataset $dataset_name \
+    --num_classes $num_classes \
     --data_root $dataset_root \
     --data_dir $dataset_dir \
+    --save_root $save_root \
+    --save_dir $save_dir \
     --loss $loss \
     --lr $lr \
     --batch_size $batch_size \
-    --epoch $epoch
-    --save_root $save_root \
-    --save_dir $save_dir \
-    --num_classes $num_classes \
-    --image_width 384 \
-    --image_height 384 \
-    --optimizer Adagrad
+    --epoch $epoch \
+    --image_width $image_width \
+    --image_height $image_height \
+    --optimizer $optimizer
