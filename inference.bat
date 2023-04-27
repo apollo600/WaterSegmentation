7bc86c65ffe8ad9cabb8503cf96219decdf24291
Tastror <yccozy2001@163.com>
2023-04-28 06:30:23 +0800

    .

--- inference.bat
+++ inference.bat
@@ -1,5 +1,5 @@
 python inference.py ^
-    --model_path ./models/2023-04-28-04-07-14_epoch-100_lr-0.0005_loss-CrossEntropy_optim-Adagrad_best_acc-0.5263.pt ^
+    --model_path ./models/2023-04-28-05-58-51_epoch-100_lr-0.0005_loss-CrossEntropy_optim-Adagrad_best_acc-0.4079.pt ^
     --data_root ./ ^
     --data_dir 1945/ ^
     --log_root ./ ^
