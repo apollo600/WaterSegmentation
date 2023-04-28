170879adf66de9359a45c90547c0cbec5d2c4a93
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-28 22:50:01 +0800

    auto commit

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ from utils.kitti_dataset import KittiData
 from utils import visual
 
 from model2.deeplab_v3plus import DeepLab, weights_init
+from utils.visual import show_config
 
 def get_parser():
     import argparse
@@ -227,9 +228,31 @@ if __name__ == "__main__":
         model = UNet(3, args.num_classes)
     elif args.model == "Deeplab":
         model = DeepLab(num_classes=args.num_classes+1, backbone=args.backbone, downsample_factor=args.downsample_factor)
+        # 初始化大模型中的参数
         weights_init(model, init_type="normal")
+        # 加载预训练模型
+        model_dict = model.state_dict()
+        # 使用 map_location 直接加载到显存
+        pretrained_dict = torch.load(args.pretrain_model_path, map_location=device)
+        load_key, no_load_key, temp_dict = [], [], {}
+        for k, v in pretrained_dict.items():
+            if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
+                temp_dict[k] = v
+                load_key.append(k)
+            else:
+                no_load_key.append(k)
+        model_dict.update(temp_dict)
+        model.load_state_dict(model_dict)
+        # 输出加载预训练结果
+        print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
+        print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
+        # print("\n\033[1;33;44m温馨提示，head部分没有载入是正常现象，Backbone部分没有载入是错误的。\033[0m")
     train_model = model.train()
     train_model.cuda()
+
+    # 展示参数
+    show_config(num_classes=args.num_classes, backbone=args.backbone, pretrain_model_path=args.pretrain_model_path,
+    input_shape=(args.image_width, args.image_height), init_epoch=)
 
     # Train
     print("Start Train")
