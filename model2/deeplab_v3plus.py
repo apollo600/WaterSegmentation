3fe347e8a8dd5757986d5ab1958900c1ca2fa5b4
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-28 22:05:01 +0800

    auto commit

--- model2/deeplab_v3plus.py
+++ model2/deeplab_v3plus.py
@@ -5,6 +5,86 @@ from nets.xception import xception
 from nets.mobilenetv2 import mobilenetv2
 
 
+class DeepLab(nn.Module):
+    def __init__(self, num_classes, backbone="mobilenet", pretrained=True, downsample_factor=16):
+        super(DeepLab, self).__init__()
+        if backbone=="xception":
+            #----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,256]
+            #   主干部分    [30,30,2048]
+            #----------------------------------#
+            self.backbone = xception(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 2048
+            low_level_channels = 256
+        elif backbone=="mobilenet":
+            #----------------------------------#
+            #   获得两个特征层
+            #   浅层特征    [128,128,24]
+            #   主干部分    [30,30,320]
+            #----------------------------------#
+            self.backbone = MobileNetV2(downsample_factor=downsample_factor, pretrained=pretrained)
+            in_channels = 320
+            low_level_channels = 24
+        else:
+            raise ValueError('Unsupported backbone - `{}`, Use mobilenet, xception.'.format(backbone))
+
+        #-----------------------------------------#
+        #   ASPP特征提取模块
+        #   利用不同膨胀率的膨胀卷积进行特征提取
+        #   相当于 Encoder，可以无视分辨率吗
+        #-----------------------------------------#
+        self.aspp = ASPP(dim_in=in_channels, dim_out=256, rate=16//downsample_factor)
+        
+        #----------------------------------#
+        #   浅层特征边
+        #   直接接到最后的 Decoder，不经过大量卷积，保留原始特征
+        #----------------------------------#
+        self.shortcut_conv = nn.Sequential(
+            nn.Conv2d(low_level_channels, 48, 1),
+            nn.BatchNorm2d(48),
+            nn.ReLU(inplace=True)
+        )		
+
+        # 用于拼接，注意这里使用了大量的 Dropout
+        self.cat_conv = nn.Sequential(
+            nn.Conv2d(48+256, 256, 3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(256, 256, 3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(0.1),
+        )
+        self.cls_conv = nn.Conv2d(256, num_classes, 1, stride=1)
+
+    def forward(self, x):
+        # x: C, H, W
+        H, W = x.size(2), x.size(3)
+        
+        #-----------------------------------------#
+        #   获得两个特征层
+        #   low_level_features: 浅层特征-进行卷积处理
+        #   x : 主干部分-利用ASPP结构进行加强特征提取
+        #-----------------------------------------#
+        low_level_features, x = self.backbone(x)
+        x = self.aspp(x)
+        low_level_features = self.shortcut_conv(low_level_features)
+        
+        #-----------------------------------------#
+        #   将加强特征边上采样
+        #   与浅层特征堆叠后利用卷积进行特征提取
+        #-----------------------------------------#
+        x = F.interpolate(x, size=(low_level_features.size(2), low_level_features.size(3)), mode='bilinear', align_corners=True)
+        x = self.cat_conv(torch.cat((x, low_level_features), dim=1))
+        x = self.cls_conv(x)
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+        return x
+
+
 class MobileNetV2(nn.Module):
     def __init__(self, downsample_factor=8, pretrained=True):
         super(MobileNetV2, self).__init__()
@@ -113,79 +193,3 @@ class ASPP(nn.Module):
 		feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
 		result = self.conv_cat(feature_cat)
 		return result
-
-
-class DeepLab(nn.Module):
-    def __init__(self, num_classes, backbone="mobilenet", pretrained=True, downsample_factor=16):
-        super(DeepLab, self).__init__()
-        if backbone=="xception":
-            #----------------------------------#
-            #   获得两个特征层
-            #   浅层特征    [128,128,256]
-            #   主干部分    [30,30,2048]
-            #----------------------------------#
-            self.backbone = xception(downsample_factor=downsample_factor, pretrained=pretrained)
-            in_channels = 2048
-            low_level_channels = 256
-        elif backbone=="mobilenet":
-            #----------------------------------#
-            #   获得两个特征层
-            #   浅层特征    [128,128,24]
-            #   主干部分    [30,30,320]
-            #----------------------------------#
-            self.backbone = MobileNetV2(downsample_factor=downsample_factor, pretrained=pretrained)
-            in_channels = 320
-            low_level_channels = 24
-        else:
-            raise ValueError('Unsupported backbone - `{}`, Use mobilenet, xception.'.format(backbone))
-
-        #-----------------------------------------#
-        #   ASPP特征提取模块
-        #   利用不同膨胀率的膨胀卷积进行特征提取
-        #-----------------------------------------#
-        self.aspp = ASPP(dim_in=in_channels, dim_out=256, rate=16//downsample_factor)
-        
-        #----------------------------------#
-        #   浅层特征边
-        #----------------------------------#
-        self.shortcut_conv = nn.Sequential(
-            nn.Conv2d(low_level_channels, 48, 1),
-            nn.BatchNorm2d(48),
-            nn.ReLU(inplace=True)
-        )		
-
-        self.cat_conv = nn.Sequential(
-            nn.Conv2d(48+256, 256, 3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-
-            nn.Conv2d(256, 256, 3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-
-            nn.Dropout(0.1),
-        )
-        self.cls_conv = nn.Conv2d(256, num_classes, 1, stride=1)
-
-    def forward(self, x):
-        H, W = x.size(2), x.size(3)
-        #-----------------------------------------#
-        #   获得两个特征层
-        #   low_level_features: 浅层特征-进行卷积处理
-        #   x : 主干部分-利用ASPP结构进行加强特征提取
-        #-----------------------------------------#
-        low_level_features, x = self.backbone(x)
-        x = self.aspp(x)
-        low_level_features = self.shortcut_conv(low_level_features)
-        
-        #-----------------------------------------#
-        #   将加强特征边上采样
-        #   与浅层特征堆叠后利用卷积进行特征提取
-        #-----------------------------------------#
-        x = F.interpolate(x, size=(low_level_features.size(2), low_level_features.size(3)), mode='bilinear', align_corners=True)
-        x = self.cat_conv(torch.cat((x, low_level_features), dim=1))
-        x = self.cls_conv(x)
-        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
-        return x
-
