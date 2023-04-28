0bb0149d68de15b248cc3870ea087c381b8fecd5
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-28 22:40:01 +0800

    auto commit

--- model2/deeplab_v3plus.py
+++ model2/deeplab_v3plus.py
@@ -64,7 +64,7 @@ class DeepLab(nn.Module):
     def forward(self, x):
         # x: C, H, W
         H, W = x.size(2), x.size(3)
-        
+
         #-----------------------------------------#
         #   获得两个特征层
         #   low_level_features: 浅层特征-进行卷积处理
@@ -193,3 +193,26 @@ class ASPP(nn.Module):
 		feature_cat = torch.cat([conv1x1, conv3x3_1, conv3x3_2, conv3x3_3, global_feature], dim=1)
 		result = self.conv_cat(feature_cat)
 		return result
+
+
+def weights_init(net, init_type='normal', init_gain=0.02):
+    
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and classname.find('Conv') != -1:
+            if init_type == 'normal':
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                torch.nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+        elif classname.find('BatchNorm2d') != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    
+    print('initialize network with %s type' % init_type)
+    net.apply(init_func)
