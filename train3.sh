72d8ddc82c6d5ad17922d35f107f78e784b37f60
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-29 05:00:02 +0800

    auto commit

--- train3.sh
+++ train3.sh
@@ -28,7 +28,7 @@ python train.py \
     --weight_decay 0 \
     --lr_decay_type cos \
     --focal_loss \
-    --class_weights 1 1 50 50 100 1
+    --class_weights 1 1 2 2 4 1
     # --loss "" \
     # --batch_size \
     # --epoch \
