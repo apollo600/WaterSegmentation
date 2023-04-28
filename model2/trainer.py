de074c1b48fbbdbf0239a2d8ae08f5151970a08a
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-29 00:20:02 +0800

    auto commit

--- model2/trainer.py
+++ model2/trainer.py
@@ -27,6 +27,9 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
     batch_size = args.freeze_batch_size
     Init_lr = args.lr
     Min_lr = args.min_lr
+    Init_Epoch = args.init_epoch
+    UnFreeze_Epoch = args.unfreeze_epoch
+    UnFreeze_flag = False
 
     # -------------------------------------------------------------------#
     #   判断当前batch_size，自适应调整学习率
@@ -65,6 +68,66 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
                                         eval_flag=eval_flag, period=eval_period)
     else:
         eval_callback   = None
+
+    #---------------------------------------#
+    #   开始模型训练
+    #---------------------------------------#
+    for epoch in range(Init_Epoch, UnFreeze_Epoch):
+        #---------------------------------------#
+        #   如果模型有冻结学习部分
+        #   则解冻，并设置参数
+        #---------------------------------------#
+        if epoch >= Freeze_Epoch and not UnFreeze_flag:
+            batch_size = Unfreeze_batch_size
+
+            #-------------------------------------------------------------------#
+            #   判断当前batch_size，自适应调整学习率
+            #-------------------------------------------------------------------#
+            nbs             = 16
+            lr_limit_max    = 5e-4 if optimizer_type == 'adam' else 1e-1
+            lr_limit_min    = 3e-4 if optimizer_type == 'adam' else 5e-4
+            if backbone == "xception":
+                lr_limit_max    = 1e-4 if optimizer_type == 'adam' else 1e-1
+                lr_limit_min    = 1e-4 if optimizer_type == 'adam' else 5e-4
+            Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
+            Min_lr_fit      = min(max(batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
+            #---------------------------------------#
+            #   获得学习率下降的公式
+            #---------------------------------------#
+            lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
+                
+            for param in model.backbone.parameters():
+                param.requires_grad = True
+                        
+            epoch_step      = num_train // batch_size
+            epoch_step_val  = num_val // batch_size
+
+            if epoch_step == 0 or epoch_step_val == 0:
+                raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
+
+            if distributed:
+                batch_size = batch_size // ngpus_per_node
+
+            gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
+                                        drop_last = True, collate_fn = deeplab_dataset_collate, sampler=train_sampler)
+            gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
+                                        drop_last = True, collate_fn = deeplab_dataset_collate, sampler=val_sampler)
+
+            UnFreeze_flag = True
+
+        if distributed:
+            train_sampler.set_epoch(epoch)
+
+        set_optimizer_lr(optimizer, lr_scheduler_func, epoch)
+
+        fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, epoch, 
+                epoch_step, epoch_step_val, gen, gen_val, UnFreeze_Epoch, Cuda, dice_loss, focal_loss, cls_weights, num_classes, fp16, scaler, save_period, save_dir, local_rank)
+
+        if distributed:
+            dist.barrier()
+
+    if local_rank == 0:
+        loss_history.writer.close()
 
 
 def get_lr_scheduler(lr_decay_type, lr, min_lr, total_iters, warmup_iters_ratio=0.1, warmup_lr_ratio=0.1, no_aug_iter_ratio=0.3, step_num=10):
