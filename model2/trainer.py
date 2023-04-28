5e6270cd8ce82b329c8d248a6d0ffadbad20b60c
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-29 00:25:02 +0800

    auto commit

--- model2/trainer.py
+++ model2/trainer.py
@@ -20,7 +20,7 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
     #   提示OOM或者显存不足请调小Batch_size
     # ------------------------------------------------------#
 
-    # 冻结一定部分
+    # 冻结主体部分
     for param in model.backbone.parameters():
         param.requires_grad = False
 
@@ -28,6 +28,7 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
     Init_lr = args.lr
     Min_lr = args.min_lr
     Init_Epoch = args.init_epoch
+    Freeze_Epoch = args.freeze_epoch
     UnFreeze_Epoch = args.unfreeze_epoch
     UnFreeze_flag = False
 
@@ -74,8 +75,7 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
     #---------------------------------------#
     for epoch in range(Init_Epoch, UnFreeze_Epoch):
         #---------------------------------------#
-        #   如果模型有冻结学习部分
-        #   则解冻，并设置参数
+        #   如果完成了冻结学习部分，则解冻，并设置参数
         #---------------------------------------#
         if epoch >= Freeze_Epoch and not UnFreeze_flag:
             batch_size = Unfreeze_batch_size
@@ -84,11 +84,11 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
             #   判断当前batch_size，自适应调整学习率
             #-------------------------------------------------------------------#
             nbs             = 16
-            lr_limit_max    = 5e-4 if optimizer_type == 'adam' else 1e-1
-            lr_limit_min    = 3e-4 if optimizer_type == 'adam' else 5e-4
-            if backbone == "xception":
-                lr_limit_max    = 1e-4 if optimizer_type == 'adam' else 1e-1
-                lr_limit_min    = 1e-4 if optimizer_type == 'adam' else 5e-4
+            lr_limit_max    = 5e-4 if args.optimizer == 'Adam' else 1e-1
+            lr_limit_min    = 3e-4 if args.optimizer == 'Adam' else 5e-4
+            if backbone == "Xception":
+                lr_limit_max    = 1e-4 if args.optimizer == 'Adam' else 1e-1
+                lr_limit_min    = 1e-4 if args.optimizer == 'Adam' else 5e-4
             Init_lr_fit     = min(max(batch_size / nbs * Init_lr, lr_limit_min), lr_limit_max)
             Min_lr_fit      = min(max(batch_size / nbs * Min_lr, lr_limit_min * 1e-2), lr_limit_max * 1e-2)
             #---------------------------------------#
@@ -96,27 +96,17 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
             #---------------------------------------#
             lr_scheduler_func = get_lr_scheduler(lr_decay_type, Init_lr_fit, Min_lr_fit, UnFreeze_Epoch)
                 
+            # 进入解冻模式，并在此解冻
             for param in model.backbone.parameters():
                 param.requires_grad = True
                         
-            epoch_step      = num_train // batch_size
-            epoch_step_val  = num_val // batch_size
+            epoch_step      = train_size // batch_size
+            epoch_step_val  = val_size // batch_size
 
             if epoch_step == 0 or epoch_step_val == 0:
                 raise ValueError("数据集过小，无法继续进行训练，请扩充数据集。")
 
-            if distributed:
-                batch_size = batch_size // ngpus_per_node
-
-            gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
-                                        drop_last = True, collate_fn = deeplab_dataset_collate, sampler=train_sampler)
-            gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
-                                        drop_last = True, collate_fn = deeplab_dataset_collate, sampler=val_sampler)
-
             UnFreeze_flag = True
-
-        if distributed:
-            train_sampler.set_epoch(epoch)
 
         set_optimizer_lr(optimizer, lr_scheduler_func, epoch)
 
