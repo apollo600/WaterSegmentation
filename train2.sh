bb8856fd22882e3a9035c23f2e90f0506b4500bc
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-28 23:05:01 +0800

    auto commit

---
+++ train2.sh
@@ -0,0 +1,17 @@
+python train.py \
+--dataset My \
+--num_classes 6 \
+--data_root "/home/data/" \
+--data_dir "1945" \
+--save_root "/project/train/" \
+--save_dir "models" \
+--log_root "/project/train" \
+--log_dir "log" \
+# --loss "" \
+--lr "5e-4" \
+# --batch_size \
+# --epoch \
+--image_width 512 \
+--image_height 512 \
+--optimizer "Adam" \
+
