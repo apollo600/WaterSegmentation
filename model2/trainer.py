5f928ccdea91b58f49db12364cd5bfe97178d05a
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-29 00:35:01 +0800

    auto commit

--- model2/trainer.py
+++ model2/trainer.py
@@ -7,6 +7,7 @@ from PIL import Image
 from torch.utils.data.dataloader import DataLoader
 import torch.nn as nn
 from model2.utils.callbacks import EvalCallback
+from model2.loss import Dice_loss, CE_Loss, Focal_Loss
 
 
 def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_model: nn.Module, args, optimizer, train_size, val_size):
@@ -117,7 +118,7 @@ def Deeplab_trainer(train_loader: DataLoader, val_loader: DataLoader, train_mode
     #     loss_history.writer.close()
 
 
-def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, epoch, epoch_step, epoch_step_val, 
+def fit_one_epoch(train_model, model, loss_history, eval_callback, optimizer, epoch, epoch_step, epoch_step_val, 
                   train_loader, gen_val, Epoch, cuda, dice_loss, focal_loss, cls_weights, num_classes, fp16, scaler, 
                   save_period, save_dir, args):
                                             
@@ -129,7 +130,7 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
 
     print('Start Train')
     pbar = tqdm(total=epoch_step, desc=f'Epoch {epoch + 1}/{Epoch}', postfix=dict, mininterval=0.3)
-    model_train.train()
+    train_model.train()
 
     for iteration, batch in enumerate(train_loader):
         if iteration >= epoch_step: 
@@ -138,73 +139,43 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
 
         with torch.no_grad():
             weights = torch.from_numpy(args.cls_weights)
-            imgs    = imgs.cuda(local_rank)
-            pngs    = pngs.cuda(local_rank)
-            labels  = labels.cuda(local_rank)
-            weights = weights.cuda(local_rank)
+            imgs    = imgs.cuda()
+            pngs    = pngs.cuda()
+            labels  = labels.cuda()
+            weights = weights.cuda()
         #----------------------#
         #   清零梯度
         #----------------------#
         optimizer.zero_grad()
-        if not fp16:
-            #----------------------#
-            #   前向传播
-            #----------------------#
-            outputs = model_train(imgs)
-            #----------------------#
-            #   计算损失
-            #----------------------#
-            if focal_loss:
-                loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
-            else:
-                loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
-
-            if dice_loss:
-                main_dice = Dice_loss(outputs, labels)
-                loss      = loss + main_dice
-
-            with torch.no_grad():
-                #-------------------------------#
-                #   计算f_score
-                #-------------------------------#
-                _f_score = f_score(outputs, labels)
-
-            #----------------------#
-            #   反向传播
-            #----------------------#
-            loss.backward()
-            optimizer.step()
+        
+        #!!! 原代码这里有针对半精度浮点数到选项，这里直接简化掉了
+        #----------------------#
+        #   前向传播
+        #----------------------#
+        outputs = train_model(imgs)
+        #----------------------#
+        #   计算损失
+        #----------------------#
+        if args.focal_loss:
+            loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
         else:
-            from torch.cuda.amp import autocast
-            with autocast():
-                #----------------------#
-                #   前向传播
-                #----------------------#
-                outputs = model_train(imgs)
-                #----------------------#
-                #   计算损失
-                #----------------------#
-                if focal_loss:
-                    loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
-                else:
-                    loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
+            loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
 
-                if dice_loss:
-                    main_dice = Dice_loss(outputs, labels)
-                    loss      = loss + main_dice
+        if args.dice_loss:
+            main_dice = Dice_loss(outputs, labels)
+            loss      = loss + main_dice
+        
+        with torch.no_grad():
+            #-------------------------------#
+            #   计算f_score
+            #-------------------------------#
+            _f_score = f_score(outputs, labels)
 
-                with torch.no_grad():
-                    #-------------------------------#
-                    #   计算f_score
-                    #-------------------------------#
-                    _f_score = f_score(outputs, labels)
-                    
-            #----------------------#
-            #   反向传播
-            #----------------------#
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
+        #----------------------#
+        #   反向传播
+        #----------------------#
+        loss.backward()
+        optimizer.step()
 
         total_loss      += loss.item()
         total_f_score   += _f_score.item()
@@ -221,7 +192,7 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
         print('Start Validation')
         pbar = tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3)
 
-    model_train.eval()
+    train_model.eval()
     for iteration, batch in enumerate(gen_val):
         if iteration >= epoch_step_val:
             break
@@ -237,7 +208,7 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
             #----------------------#
             #   前向传播
             #----------------------#
-            outputs     = model_train(imgs)
+            outputs     = train_model(imgs)
             #----------------------#
             #   计算损失
             #----------------------#
@@ -267,7 +238,7 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
         pbar.close()
         print('Finish Validation')
         loss_history.append_loss(epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)
-        eval_callback.on_epoch_end(epoch + 1, model_train)
+        eval_callback.on_epoch_end(epoch + 1, train_model)
         print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
         print('Total Loss: %.3f || Val Loss: %.3f ' % (total_loss / epoch_step, val_loss / epoch_step_val))
         
