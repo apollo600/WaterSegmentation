94439fa49c0bce2809414f716fb5b3404ff45f21
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-29 02:15:02 +0800

    auto commit

--- model2/loss.py
+++ model2/loss.py
@@ -25,6 +25,7 @@ def Focal_Loss(inputs, target, cls_weights, num_classes=21, alpha=0.5, gamma=2):
     temp_inputs = inputs.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
     temp_target = target.view(-1)
 
+    cls_weights = torch.from_numpy(cls_weights).long()
     logpt  = -nn.CrossEntropyLoss(weight=cls_weights, ignore_index=num_classes, reduction='none')(temp_inputs, temp_target)
     pt = torch.exp(logpt)
     if alpha is not None:
