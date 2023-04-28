08ceb82631b94af18e6e9ba71df64c4ba6ac90ef
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-28 23:20:01 +0800

    auto commit

--- train2.sh
+++ train2.sh
@@ -1,21 +1,35 @@
-python train.py \
---dataset My \
---num_classes 6 \
---data_root "/home/data/" \
---data_dir "1945" \
---save_root "/project/train/" \
---save_dir "models" \
---log_root "/project/train" \
---log_dir "log" \
-# --loss "" \
---lr "5e-4" \
-# --batch_size \
-# --epoch \
---image_width 512 \
---image_height 512 \
---optimizer "Adam" \
---log_visual \
---model Deeplab \
---backbone Mobilenet \
---pretrain_model_path "/" \
+set -x
 
+python train.py \
+    --dataset My \
+    --num_classes 6 \
+    --data_root "/home/data/" \
+    --data_dir "1945" \
+    --save_root "/project/train/" \
+    --save_dir "models" \
+    --log_root "/project/train" \
+    --log_dir "log" \
+    --lr "5e-4" \
+    --image_width 512 \
+    --image_height 512 \
+    --optimizer "Adam" \
+    --log_visual \
+    --model Deeplab \
+    --backbone Mobilenet \
+    --pretrain_model_path "/project/train/models/deeplab_mobilenetv2.pth" \
+    --downsample_factor 16 \
+    --init_epoch 0 \
+    --freeze_epoch 25 \
+    --freeze_batch_size 8 \
+    --unfreeze_epoch 50 \
+    --unfreeze_batch_size 4 \
+    --min_lr 5e-6 \
+    --momentum 0.9 \
+    --weight_decay 0 \
+    --lr_decay_type cos \
+    --dice_loss \
+    --focal_loss \
+    --class_weights [1, 10, 10, 20, 1]
+    # --loss "" \
+    # --batch_size \
+    # --epoch \
