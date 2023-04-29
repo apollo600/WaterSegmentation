d818f1ef0fe58d5a3d98b0ebdf03b978c4a1dc58
cvmart-dev-59359-nanyou <cvmart-dev-59359-nanyou@cvmart.net>
2023-04-29 20:57:11 +0800

    auto commit

--- train2.sh
+++ train2.sh
@@ -16,12 +16,12 @@ python /project/train/src_repo/train.py \
     --log_visual \
     --model Deeplab \
     --backbone Mobilenet \
-    --pretrain_model_path "/project/train/models/deeplab_mobilenetv2.pth" \
+    --pretrain_model_path "/project/train/src_repo/deeplab_mobilenetv2.pth" \
     --downsample_factor 16 \
     --init_epoch 0 \
-    --freeze_epoch 2 \
+    --freeze_epoch 25 \
     --freeze_batch_size 16 \
-    --unfreeze_epoch 4 \
+    --unfreeze_epoch 50 \
     --unfreeze_batch_size 8 \
     --min_lr 5e-6 \
     --momentum 0.9 \
