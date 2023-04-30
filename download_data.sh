b66a5d56e38109dd56f42f3b2375d54a82e2e5fc
Tastror <yccozy2001@163.com>
2023-04-30 10:52:31 +0800

    merge; delete fucking auto commit; delete train2.sh

--- download_data.sh
+++
@@ -1,3 +0,0 @@
-wget https://extremevision-js-userfile.oss-cn-hangzhou.aliyuncs.com/user-59359-files/509c8770-1232-4762-a548-8cf57d1a9cc9/data_semantics.zip
-
-unzip -d data_semantics
--- inference.bat
+++
@@ -1,8 +0,0 @@
-python inference.py ^
-    --model_path ./models/2023-04-28-19-32-20_epoch-100_lr-0.0001_loss-CrossEntropy_optim-RMSprop_best_acc-0.7162.pt ^
-    --data_root ./ ^
-    --data_dir 1945/ ^
-    --log_root ./ ^
-    --log_dir log/infer/ ^
-    --mask_root ./ ^
-    --mask_dir ./
