099bb8530868d4ccf93d54f9a75c2da4feb7d031
Tastror <yccozy2001@163.com>
2023-04-30 21:56:58 +0800

    debug; add continue train

--- DeepLab/train.py
+++ DeepLab/train.py
@@ -185,16 +185,19 @@ if __name__ == "__main__":
             model_dict = model.state_dict()
             # 使用 map_location 直接加载到显存
             sys.stdout.write("Load pretrained model to device\n")
-            pretrained_dict = torch.load(args.pretrain_model_path, map_location=device)
-            load_key, no_load_key, temp_dict = [], [], {}
-            for k, v in pretrained_dict.items():
-                if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
-                    temp_dict[k] = v
-                    load_key.append(k)
-                else:
-                    no_load_key.append(k)
-            model_dict.update(temp_dict)
-            model.load_state_dict(model_dict)
+            try:
+                pretrained_dict = torch.load(args.pretrain_model_path, map_location=device)
+                load_key, no_load_key, temp_dict = [], [], {}
+                for k, v in pretrained_dict.items():
+                    if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
+                        temp_dict[k] = v
+                        load_key.append(k)
+                    else:
+                        no_load_key.append(k)
+                model_dict.update(temp_dict)
+                model.load_state_dict(model_dict)
+            except AttributeError:  # use no state_dict
+                model = torch.load(args.pretrain_model_path)
             # 输出加载预训练结果
             sys.stdout.write("\nSuccessful Load Key: {}...\nSuccessful Load Key Num: {}\n".format(str(load_key)[:500], len(load_key)))
             sys.stdout.write("\nFail Load Key: {}...\nFail Load Key Num: {}\n".format(str(no_load_key)[:500], len(no_load_key)))
