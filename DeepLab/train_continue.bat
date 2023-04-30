0d5ebd22728bdc294211dacf64cabb233efad3ef
Tastror <yccozy2001@163.com>
2023-05-01 03:45:13 +0800

    update continue

--- DeepLab/train_continue.bat
+++ DeepLab/train_continue.bat
@@ -7,21 +7,21 @@ python ./DeepLab/train.py ^
     --save_dir "models/deeplab/my/" ^
     --log_root "./" ^
     --log_dir "tensorboard" ^
-    --lr "5e-4" ^
+    --lr "1e-4" ^
     --image_width 512 ^
     --image_height 512 ^
     --optimizer "Adam" ^
     --log_visual ^
     --model Deeplab ^
     --backbone Mobilenet ^
-    --pretrain_model_path "./models/deeplab/my/last_epoch_weights.pth" ^
+    --pretrain_model_path "./models/deeplab/my/best_epoch_weights.pth" ^
     --downsample_factor 16 ^
-    --init_epoch 35 ^
+    --init_epoch 0 ^
     --freeze_epoch 25 ^
     --freeze_batch_size 8 ^
     --unfreeze_epoch 50 ^
     --unfreeze_batch_size 8 ^
-    --min_lr 5e-6 ^
+    --min_lr 5e-7 ^
     --momentum 0.9 ^
     --weight_decay 0 ^
     --lr_decay_type cos ^
--- DeepLab/train_continue.sh
+++ DeepLab/train_continue.sh
@@ -9,21 +9,21 @@ python /project/train/src_repo/DeepLab/train.py \
     --save_dir "models/deeplab/my" \
     --log_root "/project/train" \
     --log_dir "tensorboard" \
-    --lr "5e-4" \
+    --lr "1e-4" \
     --image_width 512 \
     --image_height 512 \
     --optimizer "Adam" \
     --log_visual \
     --model Deeplab \
     --backbone Mobilenet \
-    --pretrain_model_path "/project/train/models/deeplab/my/last_epoch_weights.pth" \
+    --pretrain_model_path "/project/train/models/deeplab/my/best_epoch_weights.pth" \
     --downsample_factor 16 \
-    --init_epoch 35 \
+    --init_epoch 0 \
     --freeze_epoch 25 \
     --freeze_batch_size 8 \
     --unfreeze_epoch 50 \
     --unfreeze_batch_size 8 \
-    --min_lr 5e-6 \
+    --min_lr 5e-7 \
     --momentum 0.9 \
     --weight_decay 0 \
     --lr_decay_type cos \
